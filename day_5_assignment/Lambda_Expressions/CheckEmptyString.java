98bbca5d234a7aa6cd14a6a3051ae9931f980fe1
Prasenjit Bhosale <prasenjitbhosale1234@gmail.com>
2025-08-10 13:06:02 +0530

    modified Question 3 Lambda -> Added Problem Statement

--- day_5_assignment/Lambda_Expressions/CheckEmptyString.java
+++ day_5_assignment/Lambda_Expressions/CheckEmptyString.java
@@ -2,6 +2,11 @@ package day_5_assignment.Lambda_Expressions;
 
 import java.util.function.Predicate;
 
+//3.	Check If a String Is Empty
+//Create a lambda (via a functional interface like Predicate<String>) that returns true if a given string is empty.
+//Predicate<String> isEmpty = s -> s.isEmpty();
+
+
 public class CheckEmptyString {
 	public static void main(String[] args) {
 		Predicate<String> isEmpty = s -> s.isEmpty();
